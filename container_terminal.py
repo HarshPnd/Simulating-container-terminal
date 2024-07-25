de5ec44d0f0a4c3e30ef662ba25883c9d63c4c71
HarshPnd <harshpnd2002@gmail.com>
2024-07-25 16:06:27 +0530

    handled the truck and crane requests properly

--- container_terminal.py
+++ container_terminal.py
@@ -10,6 +10,7 @@ class ContainerTerminal:
         self.berths = simpy.Resource(environment, NUMBER_OF_BERTHS)
         self.cranes = simpy.Resource(environment, NUMBER_OF_CRANES)
         self.trucks = simpy.Resource(environment, NUMBER_OF_TRUCKS)
+        self.container_queue = simpy.Store(environment)
 
     def berth_vessel(self, vessel_id):
         """Simulate the berthing process of a vessel."""
@@ -22,19 +23,27 @@ class ContainerTerminal:
     def unload_vessel(self, vessel_id):
         """Simulate the unloading process of a vessel."""
         for container_number in range(1, CONTAINERS_PER_VESSEL + 1):
-            yield self.environment.process(self.move_container(vessel_id, container_number))
+            yield self.environment.process(self.unload_container(vessel_id, container_number))
 
-    def move_container(self, vessel_id, container_number):
-        """Simulate the process of moving a container."""
-        # Request a crane
+    def unload_container(self, vessel_id, container_number):
+        """Simulate the process of unloading a container."""
         with self.cranes.request() as crane_request:
             yield crane_request
             print(f"Time {int(self.environment.now)}: Crane starts moving container {container_number} from vessel {vessel_id}")
             yield self.environment.timeout(CONTAINER_TRANSFER_TIME)
+            print(f"Time {int(self.environment.now)}: Crane finished moving container {container_number} from vessel {vessel_id}")
+            # Add the unloaded container to the container queue
+            yield self.container_queue.put((vessel_id, container_number))
+            # Start the transport process for this container
+            self.environment.process(self.transport_container())
 
-        # Request a truck
-        with self.trucks.request() as truck_request:
-            yield truck_request
-            print(f"Time {int(self.environment.now)}: Truck picks up container {container_number} from vessel {vessel_id}")
-            yield self.environment.timeout(TRUCK_CYCLE_TIME)
-            print(f"Time {int(self.environment.now)}: Truck drops off container {container_number} from vessel {vessel_id}")
+    def transport_container(self):
+        """Simulate the transport of a container by a truck."""
+        while True:
+            # Wait for a container to be available in the queue
+            vessel_id, container_number = yield self.container_queue.get()
+            with self.trucks.request() as truck_request:
+                yield truck_request
+                print(f"Time {int(self.environment.now)}: Truck picks up container {container_number} from vessel {vessel_id}")
+                yield self.environment.timeout(TRUCK_CYCLE_TIME)
+                print(f"Time {int(self.environment.now)}: Truck drops off container {container_number} from vessel {vessel_id}")
